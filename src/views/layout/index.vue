58ac6247acf5582b836219d1c9b5ddbb8c53eaae
SwordsmanYao <swordsman.yao@foxmail.com>
2017-10-31 15:49:17 +0800

    'init'

---
+++ src/views/layout/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style rel="stylesheet/scss" lang="scss" scoped>
+
+</style>
